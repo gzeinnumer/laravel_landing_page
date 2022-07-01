a28580bab8e93c0dca5c3182d11fe8dff941d382
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-07-01 20:15:21 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# laravel_landing_page
+ 
---
+++ resources/views/layouts copy/master.blade.php
@@ -0,0 +1,13 @@
+<html>
+@include('layouts.partials.head')
+
+<body>
+    @include('layouts.partials.aside')
+
+    <div class="container">
+        @yield('content')
+    </div>
+    @include('layouts.partials.js')
+</body>
+
+</html>
