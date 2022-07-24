ba6892029792d9edf7f088f08f486db3b87ad97f
rafiarya17 <84889929+rafiarya17@users.noreply.github.com>
2022-07-24 19:49:01 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,6 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\KelompokController;
 use App\Http\Controllers\PenilaianController;
 use Illuminate\Support\Facades\Route;
-
-
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\TugasController;
 use App\Http\Controllers\UserController;
